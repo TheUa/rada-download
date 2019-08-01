c2d6f261e9db4939c787b836dd1bed5071339ab1
TheUa <the.dn.ua@gmail.com>
2019-08-01 17:50:25 +0300

    New app

--- src/main/java/com/example/radadownload/service/RetrieveDeputiesService.java
+++ src/main/java/com/example/radadownload/service/RetrieveDeputiesService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class RetrieveDeputiesService implements DeputiesService {
 
 
+
     public static List<DeputyPresence> retrievePresenceFromRadaWebsite() {
         List<DeputiesService> retrievePresenceFromRadaWebsite = new ArrayList<>();
         int min = 12008;
